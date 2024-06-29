cfcdb768f1e79230ddc9db69d0176dc045e0b7d8
Sedly <sedly.keith@student.moringaschool.com>
2024-06-29 21:11:25 +0300

    python-p4-routing-and-views-lab

--- server/app.py
+++ server/app.py
@@ -1,8 +1,34 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:route>')
+def print_string(route):
+    print(route)
+    return route
+
+@app.route('/count/<int:number>')
+def count(number):
+    count = '\n'.join([str(i) for i in range(number)])
+    return count
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, num2, operation):
+    operations = {
+        '+': num1 + num2,
+        '-': num1 - num2,
+        '*': num1 * num2,
+        'div': num1 / num2,
+        '%': num1 % num2
+    }
+    result = operations.get(operation)
+    if result is None:
+        return 'Operation not recognized. Please use one of the following: + - * div %'
+    return str(result)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
